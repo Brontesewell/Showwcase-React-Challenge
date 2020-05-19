2805824d82f15235ddff3f74861b0bb1441e750a
Brontesewell <brontsewell7@gmail.com>
2020-05-19 08:44:42 -0700

    onsubmit landing page going to /profile

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Logo from '../logo.png'
+import {RouteComponentProps, withRouter} from "react-router-dom";
 
-function LandingPage() {
+function LandingPage({history}: RouteComponentProps): JSX.Element {
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        history.push("/profile");
+    };
+    
     return (
       <div className="LandingPage">
                             <img src={Logo} alt="logo" id="logo"/>
                     <h4 id="tag-line">Connect, share, showcase projects, and get hired!</h4>
-                    <form>
+                    <form onSubmit={handleSubmit}>
 
                         <div id="landing-page-input">
                             <input type='text' id="firstname" name="firstName" placeholder='First Name' ></input>
