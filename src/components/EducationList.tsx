856df2c4e628b44e1c66d35f14e30f017c87ae8c
Brontesewell <brontsewell7@gmail.com>
2020-05-19 08:52:52 -0700

    added buttons to ad new education and open Modal

--- src/components/EducationList.tsx
+++ src/components/EducationList.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function EducationList() {
+
+    const createEducationHandler = () =>{
+
+    }
+
     return (
       <div className="EducationList">
-        <h1>Education List Loading</h1>
+        <div className="row">
+              
+              <div className="left-column">
+               <h2 id="education-title">Education</h2>
+               <button id="new-education-btn" className="button" onClick={createEducationHandler} >Add new Education</button> 
+                 
+              </div>
+
+              <div className="right-column">
+                <div className="welcome-title-box"><h1>Welcome to ShowwCase</h1><button className="button" onClick={createEducationHandler} >Add My Education</button></div> 
+            </div>
+        
+        </div>
+
       </div>
     );
   }
